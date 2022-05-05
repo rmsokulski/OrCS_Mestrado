972dfbc4612f9244b83533a4b0387d6147ccb0ff
rmsokulski <rodrigosokulski@hotmail.com>
2022-05-05 11:41:30 -0300

    Fix

--- processor/processor.cpp
+++ processor/processor.cpp
@@ -1732,13 +1732,12 @@ void processor_t::decode()
 									"Erro, Tentando decodificar mais uops que o maximo permitido")
 #if PROCESSOR_DEBUG
 				assert(new_uop.uop_operation != -1);
-				ORCS_PRINTF("%lu processor %lu decode(): addr %lu uop %lu %s, Type: %s, readyAt %lu, fetchBuffer: %u, decodeBuffer: %u, robUsed: %u.\n",
+				ORCS_PRINTF("%lu processor %lu decode(): addr %lu uop %lu %s, readyAt %lu, fetchBuffer: %u, decodeBuffer: %u, robUsed: %u.\n",
 							orcs_engine.get_global_cycle(),
 							this->processor_id,
 							new_uop.opcode_address,
 							new_uop.uop_number,
 							get_enum_instruction_operation_char(new_uop.uop_operation),
-							(new_uop.is_validation) ? "Val" : (new_uop.is_vectorial_part >= 0) ? "VP" : "Ins",
 							new_uop.readyAt,
 							this->fetchBuffer.get_size(),
 							this->decodeBuffer.get_size(),
@@ -1831,6 +1830,7 @@ void processor_t::rename()
 			this->decodeBuffer.front()->readyAt > orcs_engine.get_global_cycle()) &&
 			(this->vima_converter.vima_instructions_buffer.is_empty()))
 		{
+			//printf("Decode front not ready STATUS %s readyAt %lu\n", get_enum_package_state_char(this->decodeBuffer.front()->status), this->decodeBuffer.front()->readyAt);
 			break;
 		}
 
@@ -2001,7 +2001,7 @@ void processor_t::rename()
 						this->vima_converter.current_conversion->infos_remaining++;
 
 					}
-					/* Segundo load da segunda iteração, para descobrir o stride */
+					//Segundo load da segunda iteração, para descobrir o stride 
 					else if (this->vima_converter.iteration == 1 && this->vima_converter.addr[1] == uop->opcode_address && this->vima_converter.uop_id[1] == uop->uop_id) {
 						// Copia informações
 						this->vima_converter.addr[5] = uop->opcode_address;
@@ -2115,7 +2115,7 @@ void processor_t::rename()
 						uop->unique_conversion_id = this->vima_converter.current_conversion->unique_conversion_id;
 					}
 
-					/* Op da segunda iteração */
+					// Op da segunda iteração 
 					else if (this->vima_converter.iteration == 1 && this->vima_converter.addr[2] == uop->opcode_address && this->vima_converter.uop_id[2] == uop->uop_id) {
 						// Copia informações
 						this->vima_converter.addr[6] = uop->opcode_address;
@@ -2215,7 +2215,7 @@ void processor_t::rename()
 						this->vima_converter.current_conversion->infos_remaining++;
 						this->vima_converter.current_conversion->is_mov = is_mov;
 					}
-					/* Store da segunda iteração */
+					// Store da segunda iteração
 					else if (this->vima_converter.iteration == 1 && this->vima_converter.addr[3] == uop->opcode_address && this->vima_converter.uop_id[3] == uop->uop_id) {
 						// Copia informações
 						this->vima_converter.addr[7] = uop->opcode_address;
@@ -2418,8 +2418,9 @@ void processor_t::rename()
 				uop_operation == INSTRUCTION_OPERATION_HIVE_LOAD ||
 				uop_operation == INSTRUCTION_OPERATION_HIVE_STORE)
 			{
-				if (this->memory_order_buffer_hive_used >= MOB_HIVE || reorderBuffer.robUsed >= ROB_SIZE)
+				if (this->memory_order_buffer_hive_used >= MOB_HIVE || reorderBuffer.robUsed >= ROB_SIZE) {
 					break;
+				}
 
 				pos_mob = this->search_position_mob_hive(&MOB_LIMIT);
 				if (pos_mob == POSITION_FAIL)
@@ -2455,8 +2456,9 @@ void processor_t::rename()
 				uop_operation == INSTRUCTION_OPERATION_VIMA_GATHER  ||
 				uop_operation == INSTRUCTION_OPERATION_VIMA_SCATTER)
 			{
-				if (this->memory_order_buffer_vima_used >= MOB_VIMA || reorderBuffer.robUsed >= ROB_SIZE)
+				if (this->memory_order_buffer_vima_used >= MOB_VIMA || reorderBuffer.robUsed >= ROB_SIZE) {
 					break;
+				}
 				else
 				{
 					#if VIMA_DEBUG
@@ -2873,8 +2875,8 @@ void processor_t::dispatch()
 		{
 			ORCS_PRINTF("cycle %lu\n", orcs_engine.get_global_cycle())
 			ORCS_PRINTF("=================\n")
-			ORCS_PRINTF("Unified Reservations Station on use: %lu\n", urs->size())
-			ORCS_PRINTF("Trying Dispatch %s - %s\n", (rob_line->uop.is_validation) ? "Val" : (rob_line->uop.is_vectorial_part >= 0) ? "VP" : "Ins", rob_line->content_to_string().c_str())
+			ORCS_PRINTF("Unified Reservations Station on use: %lu\n", this->unified_reservation_station.size())
+			ORCS_PRINTF("Trying Dispatch - %s\n", rob_line->content_to_string().c_str())
 			ORCS_PRINTF("=================\n")
 		}
 #endif
